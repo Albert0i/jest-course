ea130e979532befec2ec5bd808ec7cf611657c30
Alberto Iong <albert0i@hotmail.com>
2024-07-23 17:07:29 +0800

    add incrby

--- test/redis.test.js
+++ test/redis.test.js
@@ -8,11 +8,24 @@ afterAll(async () => {
   });
 
 test("ping", async () => {
-    const pong = await redisClient.ping()
-    expect(pong).toBe('PONG')
+  const pong = await redisClient.ping()
+  expect(pong).toBe('PONG')
 })
 
+test("incrby", async() => {
+  const TESTKEY = 'test:counter'
+  await redisClient.set(TESTKEY, 0)
 
+  for (var i = 1; i <= 100; i++) {
+    await redisClient.incrby(TESTKEY, i)
+  }
+  const ret1 = await redisClient.get(TESTKEY)
+  expect(ret1).toBe('5050')
+
+  await redisClient.del(TESTKEY)
+  const ret2 = await redisClient.exists(TESTKEY)
+  expect(ret2).toBeFalsy()
+})
 
 /*
    ioredis
